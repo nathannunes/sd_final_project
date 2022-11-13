ce6d4fabb7a9da366a7a7da800ecf32639cabd1b
nathan <nathan-nunes@hotmail.com>
2022-11-13 14:10:10 -0500

    player mechanics

--- main.py
+++ main.py
@@ -16,16 +16,42 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('resources/player.png')
 pX = 370
 pY = 480
+dpX = 0
+dpY = 0
 
-def player():
-    screen.blit(playerImg,(pX,pY))
+def player(x,y):
+    screen.blit(playerImg,(x,y))
 
 # Loop to Game
 while activeStatus:
+    # Set RGB values
+    #screen.fill((59, 26, 2))
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print("Tiger out")
             activeStatus = False
-    # Set RGB values
-    screen.fill((59, 26, 2))
-    player()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(event.button)
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                dpY = -2
+            if event.key == pygame.K_s:
+                dpY = 2
+            if event.key == pygame.K_d:
+                dpX = 2
+            if event.key == pygame.K_a:
+                dpX = -2
+        if event.type == pygame.KEYUP:
+            if event.key in (pygame.K_w, pygame.K_s):
+                dpY = 0
+            if event.key in (pygame.K_a, pygame.K_d):
+                dpX =0
+
+    # update player coordinates
+    pX = 736 if (pX + dpX) >= 736 else (0 if (pX+dpX)<=0 else (pX+dpX))
+    pY = 536 if (pY + dpY) >= 536 else (0 if (pY+dpY)<=0 else (pY+dpY))
+    player(pX,pY)
     pygame.display.update()
