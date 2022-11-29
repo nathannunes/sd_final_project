595edfae1dc8df7795936e1c4f05d698f269fb83
rajat19961 <rajatpandey441@gmail.com>
2022-11-28 21:09:32 -0500

    trying multiple enemies

--- main.py
+++ main.py
@@ -28,10 +28,15 @@ if __name__ == "__main__":
     player = Player()
 
     # For the enemy
+    num_enemies = 1
     producer = enemy.FactoryProducer()
-    newEnemy = random.choice(["devil","ghost","joker","vampire"])
-    factory = producer.get_factory(newEnemy)
-    enemyPlayer = factory.create_enemy(newEnemy)
+    enemyPlayers = []
+    for i in range(0,num_enemies):
+        newEnemy = random.choice(["devil","ghost","joker","vampire"])
+        factory = producer.get_factory(newEnemy)
+        enemyPlayer = factory.create_enemy(newEnemy)
+        enemyPlayers.append(enemyPlayer)
+    
 
     # For the flame
     flame = Flame()
@@ -91,34 +96,39 @@ if __name__ == "__main__":
         player.pX = 736 if (player.pX + player.dpX) >= 736 else (0 if (player.pX + player.dpX) <= 0 else (player.pX + player.dpX))
         player.pY = 536 if (player.pY + player.dpY) >= 536 else (260 if (player.pY + player.dpY) <= 260 else (player.pY + player.dpY))
 
-        if player.playerState == "not dead":
+        #if player.playerState == "not dead":
+        for i in range(0,num_enemies):
             hit = player.hasHitPlayer(enemyPlayer.eX,enemyPlayer.eY,player.pX,player.pY)
             if hit:
                 enemyPlayer.eY = 2000
                 player.playerState="dead"
+                break
 
-            enemyPlayer.eX += enemyPlayer.deX
-            enemyPlayer.eY += enemyPlayer.deY
-            if enemyPlayer.eX <= 0:
-                enemyPlayer.deX = 2
-            elif enemyPlayer.eX >= 736:
-                enemyPlayer.deX = -2
+            enemyPlayers[i].eX += enemyPlayer.deX
+            enemyPlayers[i].eY += enemyPlayer.deY
+            if enemyPlayers[i].eX <= 0:
+                enemyPlayers[i].deX = 2
+            elif enemyPlayers[i].eX >= 736:
+                enemyPlayers[i].deX = -2
 
-            if enemyPlayer.eY <= 0:
-                enemyPlayer.deY = 3
-            elif enemyPlayer.eY >= 536:
-                enemyPlayer.deY = -2
+            if enemyPlayers[i].eY <= 0:
+                enemyPlayers[i].deY = 3
+            elif enemyPlayers[i].eY >= 536:
+                enemyPlayers[i].deY = -2
 
 
             # for Collision
-            collision = enemyPlayer.hasCollided(flame.fX, flame.fY)
+            collision = enemyPlayers[i].hasCollided(flame.fX, flame.fY)
             if collision:
-                enemyPlayer.eX = random.randrange(0, 736, 10)
-                enemyPlayer.eY = random.randrange(0, 268, 10)
+                enemyPlayers[i].eX = random.randrange(0, 736, 10)
+                enemyPlayers[i].eY = random.randrange(0, 268, 10)
                 producer = enemy.FactoryProducer()
                 newEnemy = random.choice(["devil","ghost","joker","vampire"])
                 factory = producer.get_factory(newEnemy)
                 enemyPlayer = factory.create_enemy(newEnemy)
+                enemyPlayer.eX = random.randrange(0, 736, 10)
+                enemyPlayer.eY = random.randrange(0, 268, 10)
+                enemyPlayers.append(enemyPlayer)
                 flame.flameState = "ready"
                 score_value += 1
 
